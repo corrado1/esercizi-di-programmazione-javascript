e8cab212878563bae54d79feb07f249a4613800a
corrado1 <corrado.tardy@gmail.com>
2017-05-02 14:29:20 +0200

    Update somma-e-media.js

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -3,3 +3,12 @@
   Scrivi un programma che prenda in input 5 numeri a tua scelta e restituisca in output la somma e la media.
   http://www.imparareaprogrammare.it
 */
+var uno= 5;
+var due= 6;
+var tre= 7;
+var quattro=8;
+var cinque=9;
+var somma=(uno+due+tre+quattro+cinque);
+var media=(somma/5);
+console.log("somma :"+somma);
+console.log("media :"+media);
