8e3fcca1d91e9c0de3dd23f7b439b79f4fc12cf3
corrado1 <corrado.tardy@gmail.com>
2017-05-02 02:10:58 +0200

    Update conta-il-tempo.js

--- esercizi-stringhe-numeri/conta-il-tempo.js
+++ esercizi-stringhe-numeri/conta-il-tempo.js
@@ -6,3 +6,15 @@
     Output: 3 ore, 29 minuti e 20 secondi.
   http://www.imparareaprogrammare.it
 */
+var input =12560;
+
+
+var oree = (input/60)/60;
+var ore=Math.floor(oree);
+var minutii =(oree-ore)*60 ;
+var minuti=Math.floor(minutii);
+var secondii =(minutii-minuti)*60;
+var secondi=Math.floor(secondii);
+console.log ("ore :"+ ore);
+console.log ("minuti :"+ minuti);
+console.log ("secondi :"+ secondi);
