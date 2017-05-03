daf91ba2526e3b0d9723f61df2fad20845aaf5cb
corrado1 <corrado.tardy@gmail.com>
2017-05-03 14:44:35 +0200

    Update fai-il-professore.js

--- esercizi-condizioni/fai-il-professore.js
+++ esercizi-condizioni/fai-il-professore.js
@@ -13,3 +13,35 @@
   Realizzare una versione con il costrutto Switch-Case.
   http://www.imparareaprogrammare.it
 */
+var voto = 21 ;
+switch (voto) {
+  case 30: console.log("eccellente");
+     break;
+  case 27: console.log("ottimo");
+    break;
+  case 28: console.log("ottimo");
+      break;
+  case 29: console.log("ottimo");
+      break;
+  case 24: console.log("distinto");
+      break;
+  case 25: console.log("distinto");
+      break;
+  case 26: console.log("distinto");
+      break;
+  case 21: console.log("buono");
+      break;
+  case 22: console.log("buono");
+      break;
+  case 23: console.log("buono");
+        break;
+        case 20: console.log("sufficente");
+        break;
+        case 19: console.log("sufficente");
+        break;
+        case 18: cosole.log("sufficente");
+        break;
+      }
+      if(voto>=30){console.log("non è un voto valido")}
+      if(voto<18){console.log("insufficente")}
+      if(voto<0){console.log("non è un voto valido")}
