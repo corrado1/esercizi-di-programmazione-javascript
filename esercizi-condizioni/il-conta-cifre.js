5182fdce4917203d259066024740bf69a356ffcd
corrado1 <corrado.tardy@gmail.com>
2017-05-03 19:45:33 +0200

    Update il-conta-cifre.js

--- esercizi-condizioni/il-conta-cifre.js
+++ esercizi-condizioni/il-conta-cifre.js
@@ -3,3 +3,9 @@
   Scrivi un programma che calcoli quante cifre sono contenute in un numero inserito in input (massimo 9999).
   http://www.imparareaprogrammare.it
 */
+var input="58538";
+var cifre= 0;
+cifre = input.length;
+if(cifre<=4){
+console.log(cifre)}
+else {console.log('numero troppo elevato')}
