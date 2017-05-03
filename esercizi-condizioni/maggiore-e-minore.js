0f0b8f2077bf910192443adc527cc9d94102b225
corrado1 <corrado.tardy@gmail.com>
2017-05-03 22:59:19 +0200

    Update maggiore-e-minore.js

--- esercizi-condizioni/maggiore-e-minore.js
+++ esercizi-condizioni/maggiore-e-minore.js
@@ -3,3 +3,16 @@
   Scrivi un programma che prenda quattro numeri in input, stampi in output il maggiore e il minore.
   http://www.imparareaprogrammare.it
 */
+var a = 6;
+var b = 10;
+var c = 15;
+var d = 2;
+if (a>b&&a>c&&a>d){console.log("questo è il numero maggiore: "+a)};
+if (b>a&&b>c&&b>d){console.log("questo è il numero maggiore : "+b)};
+if (c>a&&c>b&&c>d){console.log("questo è il numero maggiore : "+c)};
+if (d>a&&d>b&&d>c){console.log("questo è il numero maggiore : "+d)};
+
+if (a<b&&a<c&&a<d){console.log("questo è il numero minore : "+a)};
+if (b<a&&b<c&&b<d){console.log("questo è il numero minore : "+b)};
+if (c<a&&c<b&&c<d){console.log("questo è il numero minore : "+c)};
+if (d<a&&d<b&&d<c){console.log("questo è il numero minore : "+d)};
