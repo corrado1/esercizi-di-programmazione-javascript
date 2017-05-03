222d5cf45447e7256511dffb9040a92e191b626a
corrado1 <corrado.tardy@gmail.com>
2017-05-04 01:09:07 +0200

    Update ti-ricordi-le-tabelline.js

--- esercizi-cicli/ti-ricordi-le-tabelline.js
+++ esercizi-cicli/ti-ricordi-le-tabelline.js
@@ -3,3 +3,8 @@
   Scrivi un programma che stampi che prenda in input un numero e stampi la tabellina corrispondente.
   http://www.imparareaprogrammare.it
 */
+var a=1;
+for(i=1;i<11;i++)
+{console.log(a*i)
+
+}
