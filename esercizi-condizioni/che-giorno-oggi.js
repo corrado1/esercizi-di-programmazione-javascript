03e0ebad3a511eb4f4a2f28491f9c5e05280e2aa
corrado1 <corrado.tardy@gmail.com>
2017-05-03 11:18:50 +0200

    Update che-giorno-oggi.js

--- esercizi-condizioni/che-giorno-oggi.js
+++ esercizi-condizioni/che-giorno-oggi.js
@@ -12,3 +12,34 @@
   Scriverne una versione che anziché i giorni della settimana, visualizzi i nomi dei mesi.
   http://www.imparareaprogrammare.it
 */
+var numeroGiorno=99;
+switch(numeroGiorno) {
+  case 1:
+        console.log("oggi è lunedì");
+        break;
+  case 2:
+          console.log("oggi è martedì");
+          break;
+  case 3:
+          console.log("oggi è mercoledì");
+          break;
+  case 4:
+        console.log("oggi è giovedì");
+          break;
+  case 5:
+        console.log("oggi è venerdì");
+        break;
+  case 6:
+        console.log("oggi è sabato");
+        break;
+  case 7:
+         console.log("oggi è domenica");
+         break;
+
+   default :
+
+         console.log("questo no è un giorno della settimana")  ;
+
+
+
+}
