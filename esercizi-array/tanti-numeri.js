0c0bb7f34bf6a728295110785e759dc8a63ac4ca
corrado1 <corrado.tardy@gmail.com>
2017-05-05 14:18:33 +0200

    Update tanti-numeri.js

--- esercizi-array/tanti-numeri.js
+++ esercizi-array/tanti-numeri.js
@@ -4,3 +4,26 @@
   Stampa anche quanti sono i valori monori della media e quanti quelli maggiori.
   http://www.imparareaprogrammare.it
 */
+var maggiori= 0;
+var minori = 0;
+var media =0;
+var somma = 0;
+var numeri =[8,28,6,87,90,100,23];
+console.log(numeri);
+console.log("n° di cifre nella variabile n°: "+numeri.length);
+for (var i=0;i<numeri.length;i++){
+    somma=somma+numeri[i];
+}
+media = somma/numeri.length;
+console.log("la media è : "+media);
+
+for (var a=0;a<numeri.length;a++){
+   if (numeri[a]<media){console.log(numeri[a]);
+      minori=minori+1;}
+};
+for (var b=0;b<numeri.length;b++){
+   if (numeri[b]>media){
+      maggiori=maggiori+1;}
+}
+  console.log(minori+" numeri sono sotto la media");
+  console.log(maggiori+" numeri sono sopra la media");
